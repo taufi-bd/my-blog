3b58fefedee0640121cf1a56d79db4639787f95b
Md Taufikur Rahman Robin <taufi.bd@gmail.com>
2022-01-05 20:56:12 +0000

    ready to deploy

---
+++ out/_next/static/btt3xVnFWgcGQgIMOsObA/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcategories","\u002F","\u002Fposts","\u002Fsearch","\u002F404","\u002Fcategories\u002F[slug]","\u002F[slugParent]\u002F[[...slugChild]]","\u002Fposts\u002F[slug]","\u002Fauthors\u002F[slug]","\u002Fposts\u002Fpage\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
